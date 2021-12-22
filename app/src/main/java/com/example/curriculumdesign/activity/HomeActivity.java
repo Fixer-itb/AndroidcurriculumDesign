9c87fc2663cc07989f061d670e31d27868a44f0b
yuliang <969583875@qq.com>
2021-12-22 21:55:09 +0800

    "导航栏"

--- app/src/main/java/com/example/curriculumdesign/activity/HomeActivity.java
+++ app/src/main/java/com/example/curriculumdesign/activity/HomeActivity.java
@@ -1,34 +1,79 @@
 package com.example.curriculumdesign.activity;
 
 
-
-import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.curriculumdesign.R;
+import com.example.curriculumdesign.adapter.MyPagerAdapter;
+import com.example.curriculumdesign.entity.TabEntity;
+import com.example.curriculumdesign.fragment.HomeFragment;
+import com.example.curriculumdesign.fragment.MessageFragment;
+import com.example.curriculumdesign.fragment.MyFragment;
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
+import com.flyco.tablayout.listener.OnTabSelectListener;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends BaseActivity {
+
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
+    private ViewPager viewPager;
+    private CommonTabLayout commonTabLayout;
     //导航栏标题
     private String[] mTitles = {"首页", "消息", "我的"};
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-    }
+    private int[] mIconUnselectIds = {
+            R.mipmap.home_unselect, R.mipmap.collect_unselect,
+            R.mipmap.my_unselect};
+    private int[] mIconSelectIds = {
+            R.mipmap.home_selected, R.mipmap.collect_selected,
+            R.mipmap.my_selected
+    };
+    //导航栏 end
 
     @Override
     protected int initLayout() {
-        return 0;
+        return R.layout.activity_home;
     }
 
     @Override
     protected void initView() {
-
+        viewPager = findViewById(R.id.viewpager);
+        commonTabLayout = findViewById(R.id.commonTabLayout);
     }
 
     @Override
     protected void initData() {
+        mFragments.add(HomeFragment.newInstance());
+        mFragments.add(MessageFragment.newInstance());
+        mFragments.add(MyFragment.newInstance());
+        //绑定
+        for (int i = 0; i < mTitles.length; i++) {
+            mTabEntities.add(new TabEntity(mTitles[i], mIconSelectIds[i], mIconUnselectIds[i]));
+        }
+        commonTabLayout.setTabData(mTabEntities);
+        commonTabLayout.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelect(int position) {
+                //切换
+                viewPager.setCurrentItem(position);
+            }
+
+            @Override
+            public void onTabReselect(int position) {
+
+            }
+        });
+        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(),mTitles,mFragments));
+
+
+
+
+
+
+
 
     }
 }
