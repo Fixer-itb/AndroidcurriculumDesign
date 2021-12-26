3db1e8f4ca7edb35448fd6809d6c32e761527159
1584828686@qq,com <1584828686@qq.com>
2021-12-26 14:06:40 +0800

    添加了新建课程界面和新消息的item

---
+++ app/src/main/java/com/example/curriculumdesign/activity/MessageActivity.java
@@ -0,0 +1,90 @@
+package com.example.curriculumdesign.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.example.curriculumdesign.R;
+import com.example.curriculumdesign.base_ui.LoginActivity;
+
+public class MessageActivity extends BaseActivity {
+    private ImageView ivBackup;//返回图标
+    private EditText edtClassName;//课程名称
+    private EditText edtClassContent;//课程内容
+    private Button btnCreateClass;//创建课程按钮
+
+
+    @Override
+    protected int initLayout() {
+        return R.layout.activity_message
+    }
+
+    @Override
+    protected void initView() {
+        ivBackup = findViewById(R.id.iv_back_up);
+        edtClassName = findViewById(R.id.edt_courseName);
+        edtClassContent = findViewById(R.id.edt_course_content);
+        btnCreateClass = findViewById(R.id.btn_create_course);
+
+    }
+
+    @Override
+    protected void initData() {
+        ivBackup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navgateTo(LoginActivity.class);
+            }
+        });
+        //获取课程名称
+        edtClassName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        //获取课程内容
+        edtClassContent.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        btnCreateClass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navgateTo(LoginActivity.class);
+            }
+        });
+
+
+    }
+}
